fca67b3c9fdab9ce1a48778a7ae538d5dcc4aa7c
moalaacs <moalaacs@gmail.com>
2021-06-20 14:40:18 +0200

    Create SPEED.js

---
+++ SPEED.js
@@ -0,0 +1,5 @@
+(function(){var zoz=prompt("أنا ميعجبنيش السرعة المايلة دي .. أكتب السرعة الجديدة يابرو");
+if(zoz>15)var zoz=prompt("أصل أنت فاهم السرعات غلط .. أكتب تاني يانرم");
+document.getElementsByClassName('html5-main-video')[0].playbackRate=zoz;
+alert("ياسلاااام هي دي السرعة التمام");
+})();
